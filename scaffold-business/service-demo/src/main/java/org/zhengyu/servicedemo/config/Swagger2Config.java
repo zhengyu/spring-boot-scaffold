a1f68eb7ab081adb5645852d1dede6b2f8816798
zhengyu <zhengyu4767@gmail.com>
2022-08-15 11:53:31 +0800

    <feat>:<集成swagger文档>

---
+++ scaffold-business/service-demo/src/main/java/org/zhengyu/servicedemo/config/Swagger2Config.java
@@ -0,0 +1,141 @@
+package org.zhengyu.servicedemo.config;
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.RequestParameterBuilder;
+import springfox.documentation.schema.ScalarType;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.ParameterType;
+import springfox.documentation.service.RequestParameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spring.web.plugins.WebFluxRequestHandlerProvider;
+import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.lang.reflect.Field;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Configuration
+@EnableSwagger2
+@EnableKnife4j
+public class Swagger2Config {
+
+    @Value("${swagger.enable}")
+    private boolean enableSwagger;
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .enable(enableSwagger)
+                .select()
+                //为当前包下controller生成API文档
+                .apis(RequestHandlerSelectors.basePackage("org.zhengyu.servicedemo.controller"))
+                //为有@Api注解的Controller生成API文档
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                //为有@ApiOperation注解的方法生成API文档
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build()
+                .protocols(this.newHashSet("https", "http"))
+                .globalRequestParameters(getGlobalRequestParameters());
+    }
+
+    //生成全局通用参数
+    private List<RequestParameter> getGlobalRequestParameters() {
+        List<RequestParameter> parameters = new ArrayList<>();
+        parameters.add(new RequestParameterBuilder()
+                .name("org-code")
+                .description("医院代码")
+                .required(false)
+                .in(ParameterType.HEADER)
+                .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING))).required(false)
+                .build());
+
+        parameters.add(new RequestParameterBuilder()
+                .name("Authorization")
+                .description("token")
+                .required(false)
+                .in(ParameterType.HEADER)
+                .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
+                .required(false)
+                .build());
+
+        return parameters;
+    }
+
+    @SafeVarargs
+    private final <T> Set<T> newHashSet(T... ts) {
+        if (ts.length > 0) {
+            return new LinkedHashSet<>(Arrays.asList(ts));
+        }
+        return null;
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                // 设置标题
+                .title("Restful API")
+                // 服务条款
+                .termsOfServiceUrl("NO terms of service")
+                .description("<div style='font-size:14px;color:red;'>公众号服务，不允许对外暴漏，仅测试使用</div>")
+                // 作者信息
+                .contact(new Contact("朝阳医院", "", ""))
+                // 版本
+                .version("1.0")
+                //协议
+                .license("The Apache License")
+                // 协议url
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .build();
+    }
+
+    // 解决knife4j报错
+    // Failed to start bean 'documentationPluginsBootstrapper'
+    @Bean
+    public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+        return new BeanPostProcessor() {
+
+            @Override
+            public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+                if (bean instanceof WebMvcRequestHandlerProvider || bean instanceof WebFluxRequestHandlerProvider) {
+                    customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
+                }
+                return bean;
+            }
+
+            private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings) {
+                List<T> copy = mappings.stream()
+                        .filter(mapping -> mapping.getPatternParser() == null)
+                        .collect(Collectors.toList());
+                mappings.clear();
+                mappings.addAll(copy);
+            }
+
+            @SuppressWarnings("unchecked")
+            private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
+                try {
+                    Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
+                    field.setAccessible(true);
+                    return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
+                } catch (IllegalArgumentException | IllegalAccessException e) {
+                    throw new IllegalStateException(e);
+                }
+            }
+        };
+    }
+}
--- scaffold-business/service-demo/src/main/java/org/zhengyu/servicedemo/controller/DemoController.java
+++ scaffold-business/service-demo/src/main/java/org/zhengyu/servicedemo/controller/DemoController.java
@@ -1,5 +1,7 @@
 package org.zhengyu.servicedemo.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,11 @@ import org.zhengyu.common.result.Result;
 
 @RestController
 @RequestMapping("/buiness/demo")
+@Api(tags = "Demo模块")
 public class DemoController {
 
     @GetMapping("/test")
+    @ApiOperation(value = "测试")
     public Result<String> test() {
         return Result.success("ok");
     }
